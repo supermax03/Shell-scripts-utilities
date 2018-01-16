a15ff4908bfc176e3590865855d466a07c905240
supermax03 <maximiliano.bordon@gmail.com>
2018-01-16 15:02:51 -0300

    add description

--- requesttime.sh
+++ requesttime.sh
@@ -1,1 +1,6 @@
+################################################################
+#Author: Maximiliano Bordón
+#Summary: Sort the contents of the access.log by request time in descending order
+#
+################################################################
 awk '{print $NF,$0}' access.log | sort -nr| cut -f2- -d' '| more
